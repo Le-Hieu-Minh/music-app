960c29f236ed4aba3806d31976937339b50e2c44
Le Hieu Minh <afkhieu94@gmail.com>
2025-12-04 22:46:57 +0700

    update-topic update-song

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("ok");
+
+import express, { Express } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+
+import clientRoutes from "./router/client/index.router";
+dotenv.config();
+database.connect();
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static(`public`))
+
+//clientRouter
+clientRoutes(app);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ router/client/index.router.ts
@@ -0,0 +1,10 @@
+import { Express } from "express";
+import { topicRoutes } from "./topic.router";
+import { songRoutes } from "./song.router";
+
+const clientRoutes = (app: Express): void => {
+  app.use(`/topics`, topicRoutes);
+  app.use(`/songs`, songRoutes);
+};
+
+export default clientRoutes;
