113c0fe93f38ec8e27aee761657155aee8972c0c
Le Hieu Minh <afkhieu94@gmail.com>
2025-12-06 20:17:14 +0700

    update trang detail+tinh nang like + favorite song

--- controller/client/song.controller.ts
+++ controller/client/song.controller.ts
@@ -2,9 +2,12 @@ import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
+import FavoriteSong from "../../models/favorite-song.model";
 
+
+//[GET] /songs/:slugTopic
 export const list = async (req: Request, res: Response) => {
-  console.log(req.params.slugTopic);
+
   const topic = await Topic.findOne({
     slug: req.params.slugTopic,
     status: "active",
@@ -25,10 +28,104 @@ export const list = async (req: Request, res: Response) => {
     })
     song["infoSinger"] = infoSinger;
   }
-  console.log(songs);
+
 
   res.render("client/pages/songs/list", {
     pageTitle: topic.title,
     songs: songs
   });
+};
+
+//[GET] /songs/:slugSong
+export const detail = async (req: Request, res: Response) => {
+
+  const slugSong: string = req.params.slugSong;
+
+  const song = await Song.findOne({
+    slug: slugSong,
+    status: "active",
+    deleted: false
+  })
+
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    deleted: false
+  }).select("fullName");
+
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    deleted: false
+  }).select("title")
+  const favoriteSong = await FavoriteSong.findOne({
+    songId: song.id
+  });
+
+  song["favoriteSong"] = favoriteSong ? true : false;
+
+  res.render("client/pages/songs/detail", {
+    pageTitle: "Chi tiết bài hát",
+    song: song,
+    singer: singer,
+    topic: topic
+  });
+};
+
+//[PATCH] /like/:typeLike/:idSong
+export const like = async (req: Request, res: Response) => {
+
+  const idSong: string = req.params.idSong;
+  const typeLike: string = req.params.typeLike;
+
+  const song = await Song.findOne({
+    _id: idSong,
+    deleted: false,
+    status: "active"
+  });
+  const newLike: number = typeLike == "like" ? song.like + 1 : song.like - 1;
+  await Song.updateOne({
+    _id: idSong
+  }, {
+    like: newLike
+  })
+  res.json({
+    code: 200,
+    message: "Thành công",
+    like: newLike
+  })
+
+};
+//[PATCH] /favorite/:typeFavorite/:idSong
+export const favorite = async (req: Request, res: Response) => {
+
+  const idSong: string = req.params.idSong;
+  const typeFavorite: string = req.params.typeFavorite;
+
+  switch (typeFavorite) {
+    case "favorite":
+      const existFavorite = await FavoriteSong.findOne({
+        songId: idSong
+      });
+      if (!existFavorite) {
+        const record = new FavoriteSong({
+          // userId:"",
+          songId: idSong
+        });
+        await record.save();
+      }
+      break;
+    case "unfavorite":
+      await FavoriteSong.deleteOne({
+        songId: idSong
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  res.json({
+    code: 200,
+    message: "Thành công",
+  })
+
 };
